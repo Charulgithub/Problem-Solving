5aefad5135cec9df77414d06ed17d741df1e3e85
Charulgithub <94706977+Charulgithub@users.noreply.github.com>
2023-09-01 22:36:05 +0530

    Time: 7 ms (47.77%) | Memory: 7.7 MB (65.47%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
 string reverseWords(string s) {
+     
+      if(s.size() == 0) return s;
+        stack<string> stack;
+        string result;
+        for(int i=0; i<s.size(); i++) {
+            string word;
+            if(s[i]==' ') continue; //skip spaces
+            while(i<s.size() && s[i]!=' ' ) { //store continuous letters into word
+                word += s[i]; i++;
+            }
+            stack.push(word); //push word to the stack
+        }
+        while(!stack.empty()) {
+            result += stack.top(); stack.pop();
+            if(!stack.empty()) result += " ";
+        }
+        return result;
+    }
+
+    /* ---brute force
+    
     int n = s.size();
     vector<string> v;
     string temp = "";
@@ -13,7 +34,7 @@ string reverseWords(string s) {
             temp = "";
         }
     }
-
+// to push last word
     if (!temp.empty()) {
         v.push_back(temp);
     }
@@ -27,9 +48,9 @@ string reverseWords(string s) {
             ans += " ";
         }
     }
-
     return ans;
 }
+*/
 
     // string reverseWords(string s) {
     //     int n = s.size();
