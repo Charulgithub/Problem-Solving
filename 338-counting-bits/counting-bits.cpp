bd6b5921ff50ce51bf877ef3dced71325bad7001
Charulgithub <94706977+Charulgithub@users.noreply.github.com>
2023-09-01 09:21:35 +0530

    Time: 3 ms (89.82%) | Memory: 7.9 MB (65.80%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,5 +1,20 @@
 class Solution {
-public:
+public:   
+    vector<int> countBits(int n) {
+         vector<int> result(n+1);
+         if(n==0) return result;
+         result[0] = 0; //no. of set bits in 0 is 0
+
+         for(int i = 0; i < n+1; i++ ){
+             if(i%2 == 0) result[i] = result[i/2];
+
+             else result[i] = result[i/2] + 1;
+         }
+         return result;
+    }
+
+/*   --------TC = O(logn)
+
     vector<int> countBits(int n) {
         vector<int>res(n+1, 0);
         for(int i=0; i<n+1 ; i++){
@@ -7,4 +22,5 @@ public:
         }
         return res;
     }
+    */
 };
