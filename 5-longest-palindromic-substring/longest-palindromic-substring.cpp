0c52f6143db0b851bd6ba2eea6afa9ead5321b2a
Charulgithub <94706977+Charulgithub@users.noreply.github.com>
2023-09-03 11:44:26 +0530

    Time: 242 ms (38.33%) | Memory: 22.7 MB (38.35%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+     
+    string longestPalindrome(string s) {
+    int n = s.size();
+    vector<vector<bool>> dp(n, vector<bool>(n, false));
+    int lenLPsubstr = 0;
+    int startIdx = 0;
+
+    for (int gap = 0; gap < n; gap++) {
+        for (int start = 0; start + gap < n; start++) {
+            int end = start + gap;
+            if (gap == 0) {
+                dp[start][end] = true;
+            } else if (gap == 1) {
+                if (s[start] == s[end]) dp[start][end] = true;
+            } else {
+                if (s[start] == s[end] && dp[start + 1][end - 1]) dp[start][end] = true;
+            }
+            if (dp[start][end] && gap + 1 > lenLPsubstr) {
+                lenLPsubstr = gap + 1;
+                startIdx = start;
+            }
+        }
+    }
+    return s.substr(startIdx, lenLPsubstr);
+}
+
+
+    // string longestPalindrome(string s) {
+    //     int n = s.size();
+    //     vector<vector<bool>>dp(n, vector<bool>(n));
+    //     int lenLPsubstr = 0;
+    //     for(int gap = 0; gap<n; gap++){
+    //         for(int start =0; end =gap; end<n; start++; end++){
+    //             if(gap==0){  //len =1 -->1 char
+    //                 dp[start][end] = true;
+    //             }else if(gap==1){  // len = 2 -->2 char
+    //                 if(s[start]==s[end]) dp[start][end] = true;
+    //                 else dp[start][end] = false;
+    //             }else{
+    //                 if(s[start]==s[end] && dp[start+1][end-1]==true) dp[start][end] = true;
+    //                 else dp[start][end] = false;
+    //             }
+    //             if(dp[start][end]) lenLPsubstr = gap+1;
+    //         }
+    //     }
+    //     return lenLPsubstr;
+    // }
+};
