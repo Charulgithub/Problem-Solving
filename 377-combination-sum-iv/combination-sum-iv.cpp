312ed90df2cf44c9677b87ceb79091b750530ada
Charulgithub <94706977+Charulgithub@users.noreply.github.com>
2023-09-09 23:21:09 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (46.24%) - LeetSync

---
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int target, vector<int>& dp){
+        if(target < 0) return 0;
+        if(target == 0) return 1;
+        if(nums.size() == 0) return 0;
+        if(dp[target] != -1) return dp[target];
+        int cnt = 0;
+        for(int i = 0; i < nums.size(); i++){
+            cnt += solve( nums, target-nums[i], dp);
+        }
+        return dp[target] = cnt;
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int>dp(target+1, -1);
+        return solve( nums, target, dp);
+    }
+};
