d7475519230ebc919838bf32ed54a14723b083da
Charulgithub <94706977+Charulgithub@users.noreply.github.com>
2023-10-30 19:01:06 +0530

    Time: 29 ms (24.79%) | Memory: 20.6 MB (15.89%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-    int checkPal ( int i , int j , string& s){
+    int checkPal ( int i , int j , string& s, vector<vector<int>>& dp){
         if( i >= j) return 1;
-        if( s[i] == s[j] && checkPal(i+1, j-1, s)) return 1;
+        if( dp[i][j] != -1) return dp[i][j];
+
+        if( s[i] == s[j] && checkPal(i+1, j-1, s, dp)) return dp[i][j] = 1;
         else return 0;
     }
     int countSubstrings(string s) {
         int cnt = 0 ; 
+        int n = s.size();
+        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+
         for( int i = 0 ; i < s.size() ; i++){
             for(int j = i ; j < s.size() ; j++){
-                cnt += checkPal ( i, j, s);
+                cnt += checkPal ( i, j, s, dp);
             }
         }
         return cnt;
