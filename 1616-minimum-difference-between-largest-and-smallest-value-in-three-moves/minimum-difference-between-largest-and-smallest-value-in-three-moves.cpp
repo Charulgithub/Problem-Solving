47b54154387045b6940e57dd93af7cc5ab40b329
Charulgithub <94706977+Charulgithub@users.noreply.github.com>
2024-07-03 23:13:00 +0530

    Time: 74 ms (28.72%) | Memory: 38.8 MB (15.52%) - LeetSync

---
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minDifference(vector<int>& nums) {
+        int n = nums.size();
+        int result = INT_MAX;
+        if(n <= 4) return 0;
+        sort(nums.begin(), nums.end());
+
+        result = min(result, nums[n-1] - nums[3]);
+        result = min(result, nums[n-4] - nums[0]);
+        result = min(result, nums[n-2] - nums[2]);
+        result = min(result, nums[n-3] - nums[1]);
+
+     return result;
+    }
+};
