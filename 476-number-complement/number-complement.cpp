4e4f3a05b9bbcd90e46b5170214b4c3a3c67667d
Charulgithub <94706977+Charulgithub@users.noreply.github.com>
2024-08-23 00:02:59 +0530

    Time: 3 ms (41.07%) | Memory: 7.6 MB (8.34%) - LeetSync

--- 476-number-complement/number-complement.cpp
+++ 476-number-complement/number-complement.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int findComplement(int num) {
-        int number_of_bits = (int)(log2(num)) + 1;
-        
-        for(int i = 0; i<number_of_bits; i++) {
-            num = num^(1<<i); //Take Xor to flip
+        int numBits = (int)(log2(num))+1;
+
+        for (int i = 0 ; i < numBits ; i++){
+            num = num ^ (1 << i);
         }
         return num;
     }
